88896d721787c5036672e6d591abb284170587f9
maxwillias <willias_junior@hotmail.com>
2026-06-17 21:01:28 -0300

    Initial commit

---
+++ src/test/java/site/mojodojocasahouse/casa/CasaApplicationTests.java
@@ -0,0 +1,13 @@
+package site.mojodojocasahouse.casa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
