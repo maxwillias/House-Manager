b1ccb166dbf861193061f2583ce1033e7f5f9f3d
maxwillias <willias_junior@hotmail.com>
2026-06-29 21:48:16 -0300

    :sparkles: feat: Adiciona Hello World no backend

---
+++ src/main/java/site/mojodojocasahouse/casa/controller/HelloWorlController.java
@@ -0,0 +1,17 @@
+package site.mojodojocasahouse.casa.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
+public class HelloWorlController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
