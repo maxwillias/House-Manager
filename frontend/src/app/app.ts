c3fb8a2029a6e02621f04ad4b2fd7007bee5ad87
maxwillias <willias_junior@hotmail.com>
2026-06-29 22:30:16 -0300

    :sparkles: feat: Adiciona conexão com o backend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,43 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; // 1. Importe o ChangeDetectorRef
+import { ApiService } from './services/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit {
+  mensagem = 'teste';
+
+  // 2. Injete ele aqui no seu construtor
+  constructor(private api: ApiService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    console.log('Antes');
+
+    this.api.getHello().subscribe({
+      next: (res) => {
+        console.log('NEXT');
+        console.log(res);
+
+        this.mensagem = res;
+
+        console.log('Mensagem:', this.mensagem);
+        
+        // 3. Chame essa linha logo após atualizar a variável!
+        this.cdr.detectChanges(); 
+      },
+      error: (err) => {
+        console.log('ERROR');
+        console.error(err);
+      },
+      complete: () => {
+        console.log('COMPLETE');
+      }
+    });
+
+    console.log('Depois');
+  }
 }
---
+++ frontend/src/app/services/api.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private url = 'http://localhost:8080/api/hello';
+
+  constructor(private http: HttpClient) {}
+
+  getHello(): Observable<string> {
+    return this.http.get(this.url, {
+      responseType: 'text'
+    });
+  }
+}
