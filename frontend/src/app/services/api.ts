e2cc95bd71e385a6959226a0ca9722e94ca9ad47
PedroG022 <55058149+PedroG022@users.noreply.github.com>
2026-06-30 22:32:36 -0300

    :sparkles: feat: Adiciona configuração de .env

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private url = 'http://localhost:8080/api/hello';
-
+  private readonly url = `${environment.apiUrl}/hello`;
+  
   constructor(private http: HttpClient) {}
 
   getHello(): Observable<string> {
